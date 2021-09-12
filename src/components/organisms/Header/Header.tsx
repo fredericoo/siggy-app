68b1b7b85323cde341e5383b71145f4fda450f92
Frederico Batista <rincofrederico@gmail.com>
2021-09-12 10:15:02 +0100

    refactor: remove menu until more options

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,9 +1,7 @@
-import { HStack, Box, Link as ChakraLink, Container } from '@chakra-ui/react';
+import { HStack, Box, Container } from '@chakra-ui/react';
 import Link from 'next/link';
 import Logo from '../../atoms/Logo/Logo';
 import Account from './Account';
-
-const menuOptions = [{ label: 'Companies', href: '/companies' }];
 
 const Header: React.FC = () => {
   return (
@@ -23,14 +21,6 @@ const Header: React.FC = () => {
               <Logo h="32px" w="96px" />
             </a>
           </Link>
-
-          <HStack as="ul">
-            {menuOptions.map(({ href, label }) => (
-              <Link href={href} key={label + href} passHref>
-                <ChakraLink>{label}</ChakraLink>
-              </Link>
-            ))}
-          </HStack>
 
           <Box flex="1" justifyContent="flex-end" display="flex">
             <Account />
